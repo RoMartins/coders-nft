cc1c0c9432efcb3b62a8bd44b32fe1562dc679e5
RoMartins <rodrigomartins_santos1804@hotmail.com>
2022-09-28 23:45:53 -0300

    Component Layout create

---
+++ pages/_layout.tsx
@@ -0,0 +1,19 @@
+import {ReactNode} from 'react'
+import Footer from './_footer'
+import Navbar from './_navbar'
+import Logo from './assets/Logo'
+
+
+type LayoutProps = {
+  children: ReactNode
+}
+
+export default function Layout ({children}: LayoutProps) {
+  return (
+    <div className="bg-[#1d1f2b]">
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  )
+}
---
+++ pages/_navbar.tsx
@@ -0,0 +1,14 @@
+import Logo from './assets/Logo'
+
+
+export default function Navbar () {
+  return (
+    <div>
+      <div className="h-2 px-24 py-10 flex items-center cursor-pointer">
+        <Logo />
+        <p className='ml-3 font-semibold'>Coders NFT</p>
+      </div>
+      <hr className="w-full border-[#242634]"/>
+    </div>
+  )
+}
